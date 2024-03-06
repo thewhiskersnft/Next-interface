709a45f74c1867277fc96f357bcc853676bb5d23
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2024-03-06 19:29:51 +0530

    added getTokenMetadata

---
+++ src/metaplex/getTokenMetadata.ts
@@ -0,0 +1,46 @@
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { ENV, TokenListProvider } from "@solana/spl-token-registry";
+import { AppENVConfig } from "@/global/config/config";
+
+export const getTokenMetadata = async (
+  mintAddress: PublicKey,
+  connection: Connection
+) => {
+  const metaplex = Metaplex.make(connection);
+
+  let tokenName;
+  let tokenSymbol;
+  let tokenLogo;
+
+  const metadataAccount = metaplex
+    .nfts()
+    .pdas()
+    .metadata({ mint: mintAddress });
+
+  const metadataAccountInfo = await connection.getAccountInfo(metadataAccount);
+
+  if (metadataAccountInfo) {
+    const token = await metaplex
+      .nfts()
+      .findByMint({ mintAddress: mintAddress });
+    tokenName = token.name;
+    tokenSymbol = token.symbol;
+    tokenLogo = token.json?.image;
+  } else {
+    const provider = await new TokenListProvider().resolve();
+    const tokenList = provider.filterByChainId(ENV.MainnetBeta).getList();
+    console.log(tokenList);
+    const tokenMap = tokenList.reduce((map, item) => {
+      map.set(item.address, item);
+      return map;
+    }, new Map());
+
+    const token = tokenMap.get(mintAddress.toBase58());
+
+    tokenName = token.name;
+    tokenSymbol = token.symbol;
+    tokenLogo = token.logoURI;
+  }
+  return { tokenName, tokenLogo, tokenSymbol };
+};
